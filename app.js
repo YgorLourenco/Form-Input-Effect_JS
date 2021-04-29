9872286a417b31e8255a2ccf755cebe52337f0ed
YgorLourenco <44757992+YgorLourenco@users.noreply.github.com>
2021-04-29 10:44:57 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const labels = document.querySelectorAll('.form-control label')
+
+// .map -> define a função -> define o array -> define a função que ira pegar os items do array definido -> então o .map cria um novo array com esses items
+
+labels.forEach(function(label) {
+    label.innerHTML = label.innerText
+// Split transforma um texto em array
+    .split('')
+// primeiro parametro 'letter' vai ter o valor original e o segundo parametro o 'index' e o array que vai ser processado de fato, assim o .map vai criar um novo array com um delay de transição de 50ms baseado na regra CSS do elemento dinamico
+    .map((letter,index) => 
+    `<span style="transition-delay: ${index * 50}ms">${letter}</span>`
+    )
+// join vai juntar o array novo criado pelo .map
+    .join('')
+})
